f584f6b73d54da93e3dae7de86663c2fb3e300b8
buiducthien090605 <buiducthien090605@gmail.com>
2026-05-18 18:01:44 +0700

    test(ci): trigger product service CI only

--- backend/Product_Service/server.js
+++ backend/Product_Service/server.js
@@ -1,12 +1,14 @@
 require("./tracing");
 require("dotenv").config()
 const express = require("express")
+const cors = require("cors")
 const connectDB = require("./config/db")
 
 const swaggerUI = require("swagger-ui-express")
 const swaggerJsDoc = require("swagger-jsdoc")
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 connectDB()
